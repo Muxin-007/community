4f9c2ee7d3a96a3700b51cbe24e2d8af078a9bda
Muxin-007 <360028521@qq.com>
2020-03-11 17:08:29 +0800

    extract github config from code to yml

--- src/main/java/muxin/community/community/controller/AuthorizeController.java
+++ src/main/java/muxin/community/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import muxin.community.community.dto.AccessTokeDTO;
 import muxin.community.community.dto.GithubUser;
 import muxin.community.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,14 +15,23 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientsecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state){
         AccessTokeDTO accessTokeDTO = new AccessTokeDTO();
-        accessTokeDTO.setClient_id("74692b707d8e1210b46f");
-        accessTokeDTO.setClient_secret("5e71a15104b6fedee85a475cb2868fdcaae5e26f");
+        accessTokeDTO.setClient_id(clientId);
+        accessTokeDTO.setClient_secret(clientsecret);
         accessTokeDTO.setCode(code);
-        accessTokeDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokeDTO.setRedirect_uri(redirectUri);
         accessTokeDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokeDTO);
         GithubUser user = githubProvider.getUser(accessToken);
