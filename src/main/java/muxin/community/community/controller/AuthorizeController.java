9b0477ccdbd7a497606bdd2d6721ae18caa90a37
Muxin-007 <360028521@qq.com>
2020-03-11 22:21:37 +0800

    mapper token in cookie

--- src/main/java/muxin/community/community/controller/AuthorizeController.java
+++ src/main/java/muxin/community/community/controller/AuthorizeController.java
@@ -11,7 +11,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -35,7 +36,7 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
-                           HttpServletRequest request){
+                           HttpServletResponse response){
         AccessTokeDTO accessTokeDTO = new AccessTokeDTO();
         accessTokeDTO.setClient_id(clientId);
         accessTokeDTO.setClient_secret(clientsecret);
@@ -46,14 +47,15 @@ public class AuthorizeController {
         GithubUser githubUser = githubProvider.getUser(accessToken);
         if (githubUser != null) {
             User user = new User();
+            String token = UUID.randomUUID().toString();
             user.setName(githubUser.getName());
             user.setToken(UUID.randomUUID().toString());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
-            //登陆成功 写cookie和session
-            request.getSession().setAttribute("user", githubUser);
+            response.addCookie(new Cookie("token",token));
+
             return "redirect:/";
         } else {
             //登录失败，重新登录
---
+++ src/main/java/muxin/community/community/controller/IndexController.java
@@ -0,0 +1,36 @@
+package muxin.community.community.controller;
+
+import muxin.community.community.dto.User;
+import muxin.community.community.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class IndexController {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @GetMapping("/")
+    public String index(HttpServletRequest request) {
+
+        if (request.getCookies()!=null) {
+            Cookie[] cookies= request.getCookies();
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
+                }
+            }
+        }
+        return "index";
+    }
+}
