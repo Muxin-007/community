88eadad4d3726fe39992c72e66e160bcd94cddd7
Muxin-007 <360028521@qq.com>
2020-03-11 20:15:17 +0800

    database login

--- src/main/java/muxin/community/community/controller/AuthorizeController.java
+++ src/main/java/muxin/community/community/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package muxin.community.community.controller;
 
 import muxin.community.community.dto.AccessTokeDTO;
 import muxin.community.community.dto.GithubUser;
+import muxin.community.community.dto.User;
+import muxin.community.community.mapper.UserMapper;
 import muxin.community.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -26,6 +29,9 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
@@ -37,10 +43,17 @@ public class AuthorizeController {
         accessTokeDTO.setRedirect_uri(redirectUri);
         accessTokeDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokeDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        if (user != null) {
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if (githubUser != null) {
+            User user = new User();
+            user.setName(githubUser.getName());
+            user.setToken(UUID.randomUUID().toString());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
             //登陆成功 写cookie和session
-            request.getSession().setAttribute("user", user);
+            request.getSession().setAttribute("user", githubUser);
             return "redirect:/";
         } else {
             //登录失败，重新登录
