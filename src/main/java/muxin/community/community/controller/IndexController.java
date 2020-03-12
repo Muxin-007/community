601f5430149b8b32c7ed61ff05f7fd3c7e2aaaa7
Muxin-007 <360028521@qq.com>
2020-03-12 22:16:33 +0800

    页面分页

--- src/main/java/muxin/community/community/controller/IndexController.java
+++ src/main/java/muxin/community/community/controller/IndexController.java
@@ -1,8 +1,6 @@
 package muxin.community.community.controller;
 
-import muxin.community.community.dto.QuestionDTO;
-import muxin.community.community.mapper.QuestionMapper;
-import muxin.community.community.model.Question;
+import muxin.community.community.dto.PaginationDTO;
 import muxin.community.community.model.User;
 import muxin.community.community.mapper.UserMapper;
 import muxin.community.community.service.QuestionService;
@@ -10,10 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 @Controller
 public class IndexController {
@@ -26,7 +24,9 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(HttpServletRequest request,
-                        Model model) {
+                        Model model,
+                        @RequestParam(name = "page",defaultValue = "1") Integer page,
+                        @RequestParam(name = "size",defaultValue = "2") Integer size) {
 
         if (request.getCookies()!=null) {
             Cookie[] cookies= request.getCookies();
@@ -42,8 +42,8 @@ public class IndexController {
             }
         }
 
-        List<QuestionDTO> questionList = questionService.list();
-        model.addAttribute("questions", questionList);
+        PaginationDTO pagination = questionService.list(page,size);
+        model.addAttribute("pagination", pagination);
 
         return "index";
     }
--- src/main/java/muxin/community/community/service/QuestionService.java
+++ src/main/java/muxin/community/community/service/QuestionService.java
@@ -1,11 +1,11 @@
 package muxin.community.community.service;
 
+import muxin.community.community.dto.PaginationDTO;
 import muxin.community.community.dto.QuestionDTO;
 import muxin.community.community.mapper.QuestionMapper;
 import muxin.community.community.mapper.UserMapper;
 import muxin.community.community.model.Question;
 import muxin.community.community.model.User;
-import org.apache.ibatis.annotations.Mapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,9 +22,27 @@ public class QuestionService {
     @Autowired
     private UserMapper userMapper;
 
-    public List<QuestionDTO> list() {
-        List<Question> questions = questionMapper.list();
+    public PaginationDTO list(Integer page, Integer size) {
+
+        PaginationDTO paginationDTO = new PaginationDTO();
+        Integer totalCount = questionMapper.count();
+        paginationDTO.setPagination(totalCount, page, size);
+
+
+        if (page < 1) {
+            page = 1;
+        }
+        if (page > paginationDTO.getTotalPage()) {
+            page = paginationDTO.getTotalPage();
+        }
+
+
+        Integer offset = size * (page - 1);
+
+        List<Question> questions = questionMapper.list(offset,size);
         List<QuestionDTO> questionDTOList = new ArrayList<>();
+
+
 
         for (Question question : questions) {
             User user = userMapper.findById(question.getCreator());
@@ -35,6 +53,9 @@ public class QuestionService {
             questionDTO.setUser(user);
             questionDTOList.add(questionDTO);
         }
-        return questionDTOList;
+        paginationDTO.setQuestions(questionDTOList);
+
+
+        return paginationDTO;
     }
 }
